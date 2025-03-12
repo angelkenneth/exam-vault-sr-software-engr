b3af9a0c07f61502de601ce703d5ff662c006387
Angel Kenneth Tolentino <ghelo.ace@gmail.com>
2025-03-12 15:12:09 +0800

    reset style sheet to use Simple CSS

--- auth-fe-mx/src/router/index.ts
+++ auth-fe-mx/src/router/index.ts
@@ -1,18 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ProfileView from '@/users/presentation/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      name: 'profile',
+      component: ProfileView,
     },
     {
       path: '/register',
@@ -23,11 +18,6 @@ const router = createRouter({
       path: '/sign-in',
       name: 'sign-in',
       component: () => import('../users/presentation/SignInView.vue'),
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: () => import('../users/presentation/ProfileView.vue'),
     },
   ],
 })
