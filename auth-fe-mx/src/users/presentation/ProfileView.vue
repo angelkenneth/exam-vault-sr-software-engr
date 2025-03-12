5f8e5fb2450330fce7c5b83324d5fbff2f61b3f6
Angel Kenneth Tolentino <ghelo.ace@gmail.com>
2025-03-12 14:28:28 +0800

    added spinners

--- auth-fe-mx/src/users/presentation/ProfileView.vue
+++ auth-fe-mx/src/users/presentation/ProfileView.vue
@@ -4,26 +4,50 @@ import { useRouter } from 'vue-router'
 import { cond } from 'ramda'
 import { when200, when200Json, when401 } from '@/shared/utility/when-response.ts'
 import { tryToGetMeNetwork } from '@/users/network/GetMe.ts'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import type { PublicUser } from '@/users/entity/User.ts'
 
 const router = useRouter()
 const publicUser = ref<PublicUser | null>(null)
-const isLoading = ref(true)
+const isKnowingMe = ref(false)
+const isSigningOut = ref(false)
+const isBusy = computed(() => isKnowingMe.value && isSigningOut.value)
 
-const tryToSignOut = () =>
-  tryToSignOutNetwork().then(cond([when200(() => router.push('/sign-in'))]))
+const tryToSignOut = () => {
+  if (isBusy.value) {
+    return
+  }
+  isSigningOut.value = true
+  tryToSignOutNetwork()
+    .then(cond([when200(() => router.push('/sign-in'))]))
+    .finally(() => {
+      isSigningOut.value = false
+    })
+}
 
-const tryToGetMe = () =>
-  tryToGetMeNetwork().then(
-    cond([
-      when200Json((user: PublicUser) => {
-        publicUser.value = user
-        isLoading.value = false
-      }),
-      when401(() => router.push('/sign-in')),
-    ]),
-  )
+const tryToGetMe = () => {
+  if (isBusy.value) {
+    return
+  }
+  isKnowingMe.value = true
+  tryToGetMeNetwork()
+    .then(
+      cond([
+        when200Json((user: PublicUser) => {
+          publicUser.value = user
+        }),
+        when401(() => {
+          publicUser.value = null
+          setTimeout(() => {
+            router.push('/sign-in')
+          }, 2000)
+        }),
+      ]),
+    )
+    .finally(() => {
+      isKnowingMe.value = false
+    })
+}
 
 onMounted(() => {
   tryToGetMe()
@@ -31,7 +55,18 @@ onMounted(() => {
 </script>
 
 <template>
-  <button @click="tryToSignOut">Sign Out</button>
+  <h1>
+    {{
+      isKnowingMe
+        ? 'Loading...'
+        : publicUser
+          ? `Hello, ${publicUser.username}`
+          : 'Redirecting to sign in page...'
+    }}
+  </h1>
+  <button type="button" :disabled="isBusy" @click="tryToSignOut">
+    {{ isSigningOut ? 'Signing out...' : 'Sign out' }}
+  </button>
 </template>
 
 <style></style>
--- auth-fe-mx/src/users/presentation/SignInView.vue
+++ auth-fe-mx/src/users/presentation/SignInView.vue
@@ -13,23 +13,32 @@ const username = ref('')
 const usernameError = ref('')
 const password = ref('')
 const passwordError = ref('')
+const isSigningIn = ref(false)
 
 const redirectToProfile = () => {
   router.push('/profile')
 }
 
 const tryToSignIn = () => {
+  if (isSigningIn.value) {
+    return
+  }
+  isSigningIn.value = true
   usernameError.value = ''
   passwordError.value = ''
-  tryToSignInNetwork({ username: username.value, password: password.value }).then(
-    cond([
-      when200(redirectToProfile),
-      whenZodError<SignInInput>((zodError) => {
-        usernameError.value = zodMessageFromPath(['username'], zodError)
-        passwordError.value = zodMessageFromPath(['password'], zodError)
-      }),
-    ]),
-  )
+  tryToSignInNetwork({ username: username.value, password: password.value })
+    .then(
+      cond([
+        when200(redirectToProfile),
+        whenZodError<SignInInput>((zodError) => {
+          usernameError.value = zodMessageFromPath(['username'], zodError)
+          passwordError.value = zodMessageFromPath(['password'], zodError)
+        }),
+      ]),
+    )
+    .finally(() => {
+      isSigningIn.value = false
+    })
 }
 </script>
 
@@ -37,11 +46,27 @@ const tryToSignIn = () => {
   <div class="about">
     <h1>Please Sign In</h1>
     <form @submit.prevent="tryToSignIn">
-      <input type="text" v-model="username" placeholder="Username" maxlength="32" required />
+      <input
+        type="text"
+        v-model="username"
+        placeholder="Username"
+        maxlength="32"
+        required
+        :disabled="isSigningIn"
+      />
       <div>{{ usernameError }}</div>
-      <input type="password" v-model="password" placeholder="Password" maxlength="64" required />
+      <input
+        type="password"
+        v-model="password"
+        placeholder="Password"
+        maxlength="64"
+        required
+        :disabled="isSigningIn"
+      />
       <div>{{ passwordError }}</div>
-      <button type="submit">Sign In</button>
+      <button type="submit" :disabled="isSigningIn">
+        {{ isSigningIn ? 'Signing in...' : 'Sign in' }}
+      </button>
     </form>
   </div>
 </template>
