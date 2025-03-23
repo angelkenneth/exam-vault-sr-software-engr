1ee15f0bc424d10f14bc7601e7afb31cc6f6c867
Angel Kenneth Tolentino <ghelo.ace@gmail.com>
2025-03-24 01:49:18 +0800

    anticipated empty string input

--- contact-be-mx/src/lib/shared/local/get-json.ts
+++ contact-be-mx/src/lib/shared/local/get-json.ts
@@ -1,10 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const getJson = async <T>(req: NextRequest): Promise<T> => {
+export const getJson = async <TInput>(req: NextRequest): Promise<TInput> => {
   try {
     return await req.json();
   } catch (error) {
     if (error instanceof SyntaxError) {
+      const text = await req.text();
+      if (text === '') {
+        return {} as TInput;
+      }
       throw NextResponse.json(
         { error: 'Unsupported media type' },
         { status: 415 }
