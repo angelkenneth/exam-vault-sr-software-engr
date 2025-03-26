84972e43e8f72e7f5247abce45828430e4176b63
Angel Kenneth Tolentino <ghelo.ace@gmail.com>
2025-03-26 22:36:05 +0800

    reformat

--- auth-be-mx/src/app/users/_local/hash-password.ts
+++ auth-be-mx/src/app/users/_local/hash-password.ts
@@ -1,10 +1,10 @@
-import { scryptSync, randomBytes } from "crypto";
+import { scryptSync, randomBytes } from 'crypto';
 
 /**
  * Ref: https://stackoverflow.com/a/67038052/912215
  */
 export const hashPassword = (password: string): string => {
-  const salt = randomBytes(16).toString("hex");
-  const buf = scryptSync(password, salt, 64)
-  return `${buf.toString("hex")}.${salt}`;
+  const salt = randomBytes(16).toString('hex');
+  const buf = scryptSync(password, salt, 64);
+  return `${buf.toString('hex')}.${salt}`;
 };
--- auth-be-mx/src/app/users/_local/verify-password.ts
+++ auth-be-mx/src/app/users/_local/verify-password.ts
@@ -1,4 +1,4 @@
-import { scryptSync, timingSafeEqual } from "crypto";
+import { scryptSync, timingSafeEqual } from 'crypto';
 
 /**
  * Ref: https://stackoverflow.com/a/67038052/912215
@@ -7,8 +7,8 @@ export const verifyPassword = (
   storedPassword: string,
   suppliedPassword: string
 ): boolean => {
-  const [hashedPassword, salt] = storedPassword.split(".");
-  const hashedPasswordBuf = Buffer.from(hashedPassword, "hex");
+  const [hashedPassword, salt] = storedPassword.split('.');
+  const hashedPasswordBuf = Buffer.from(hashedPassword, 'hex');
   const suppliedPasswordBuf = scryptSync(suppliedPassword, salt, 64);
   return timingSafeEqual(hashedPasswordBuf, suppliedPasswordBuf);
 };
